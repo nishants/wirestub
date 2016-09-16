112bfb799bba1660a57547b87bea0d530244a1bd
nishant <nishant.singh87@gmail.com>
2016-09-16 20:01:57 +0530

    disable runtime args.

--- src/test/java/social/amoeba/jeyson/wiremock/RuntimeArgsTest.java
+++ src/test/java/social/amoeba/jeyson/wiremock/RuntimeArgsTest.java
@@ -1,23 +1,33 @@
-package social.amoeba.jeyson.wiremock;
-
-import org.junit.Test;
-
-import javax.script.ScriptException;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-public class RuntimeArgsTest {
-
-  @Test
-  public void addProperties() throws InterruptedException, NoSuchMethodException, ScriptException, IOException, URISyntaxException {
-    String[] args     = {"--verbose","--port", "5123"},
-             expected = {"--verbose", "--port", "5123", "--extensions", "social.amoeba.jeyson.wiremock.JeysonWiremock", "--root-dir", "/Users/dawn/Documents/projects/jeyson-java"},
-             actual   = RuntimeArgs.set(args);
-
-    assertThat(actual, is(expected));
-
-  }
-}
+//package social.amoeba.jeyson.wiremock;
+//
+//import org.junit.Test;
+//import social.amoeba.TestSupport;
+//
+//import javax.script.ScriptException;
+//import java.io.IOException;
+//import java.net.URISyntaxException;
+//
+//import static org.hamcrest.CoreMatchers.is;
+//import static org.hamcrest.MatcherAssert.assertThat;
+//
+//public class RuntimeArgsTest {
+//
+//  @Test
+//  public void shouldAddRootDirIfNotProvided() throws InterruptedException, NoSuchMethodException, ScriptException, IOException, URISyntaxException {
+//    String[] args     = {"--verbose","--port", "5123"},
+//             expected = {"--verbose", "--port", "5123", "--extensions", "social.amoeba.jeyson.wiremock.JeysonWiremock", "--root-dir", TestSupport.jarRooot()},
+//             actual   = RuntimeArgs.set(args);
+//
+//    assertThat(actual, is(expected));
+//
+//  }
+//  @Test
+//  public void shouldNotSetRootDirIfProvided() throws InterruptedException, NoSuchMethodException, ScriptException, IOException, URISyntaxException {
+//    String[] args     = {"--verbose","--port", "5123", "--root-dir", "some-path"},
+//             expected = {"--verbose","--port", "5123", "--root-dir", "some-path", "--extensions", "social.amoeba.jeyson.wiremock.JeysonWiremock"},
+//             actual   = RuntimeArgs.set(args);
+//
+//    assertThat(actual, is(expected));
+//
+//  }
+//}
