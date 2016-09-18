500d43f8306f94f9209c9c0976a1236cf56bda9e
nishant <nishant.singh87@gmail.com>
2016-09-19 03:11:04 +0530

    fix runtime args.

--- src/main/java/social/amoeba/jeyson/wiremock/RuntimeArgs.java
+++ src/main/java/social/amoeba/jeyson/wiremock/RuntimeArgs.java
@@ -2,25 +2,23 @@ package social.amoeba.jeyson.wiremock;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import static java.util.stream.IntStream.range;
+import static java.util.Arrays.asList;
 
 public class RuntimeArgs {
   public static String[] set(String[] args) throws URISyntaxException {
-    String[] params = new String[]{
-        "--extensions",
-        JeysonWiremock.class.getName(),
-        "--root-dir",
-        new File(RuntimeArgs.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParent()
-    };
-    String[] enhanced = new String[args.length  + params.length];
+    List<String> params = new ArrayList<String>(asList(args));
+    params.add("--extensions");
+    params.add(JeysonWiremock.class.getName());
 
-    range(0, args.length)
-        .forEach(index -> enhanced[index] = args[index]);
+    if(params.indexOf("--root-dir") < 0){
+      params.add("--root-dir");
+      params.add(new File(RuntimeArgs.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getAbsolutePath());
+    }
 
-    range(0, params.length)
-        .forEach(index -> enhanced[args.length + index] = params[index]);
-
-    return enhanced;
+    return params.toArray(new String[0]);
   }
 }
--- src/test/java/social/amoeba/jeyson/wiremock/RuntimeArgsTest.java
+++ src/test/java/social/amoeba/jeyson/wiremock/RuntimeArgsTest.java
@@ -15,7 +15,7 @@ public class RuntimeArgsTest {
   @Test
   public void shouldAddRootDirIfNotProvided() throws InterruptedException, NoSuchMethodException, ScriptException, IOException, URISyntaxException {
     String[] args     = {"--verbose","--port", "5123"},
-             expected = {"--verbose", "--port", "5123", "--extensions", "social.amoeba.jeyson.wiremock.JeysonWiremock", "--root-dir", TestSupport.jarRooot()},
+             expected = {"--verbose", "--port", "5123", "--extensions", "social.amoeba.jeyson.wiremock.JeysonWiremock", "--root-dir", TestSupport.jarRooot() +"/target"},
              actual   = RuntimeArgs.set(args);
 
     assertThat(actual, is(expected));
